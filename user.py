09148bc8881429e1e22392ce62902e522ab9a48c
Mindi Garcia <mgarcia410.mg@gmail.com>
2023-05-08 21:35:09 -0400

    created class

---
+++ user.py
@@ -0,0 +1,13 @@
+class User:
+    def __init__(self, first_name, last_name, email, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.age = age
+        self.is_rewards_member = False
+        self.gold_card_points = 0
+
+
+#trying to updat this to commit
+
+
