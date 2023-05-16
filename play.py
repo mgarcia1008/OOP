017c10c57445d31a654db8f610bad0f165c66fb2
Mindi Garcia <mgarcia410.mg@gmail.com>
2023-05-15 20:57:50 -0400

    completed up to second task

--- play.py
+++ play.py
@@ -1,4 +1,4 @@
-class BankAccount:
+'''class BankAccount:
     # class attributes
     bank_name = "First National Dojo"
     # new class attribute - a list of all the accounts!
@@ -23,5 +23,44 @@ class BankAccount:
 
 checking = BankAccount(.10, 500)
 checking.change_bank_name('Bank of America')
-print(checking.change_bank_name)
+print(checking.change_bank_name)'''
 
+
+'''int_to_float = float(35)
+print(int_to_float)
+float_to_int = int(44.2)
+print(float_to_int)
+in_to_complex = complex(35)
+print(in_to_complex)
+
+import random
+print(random.randint(2,5))'''
+
+# Practice Challenge: Correct the errors!
+first_name = "Alana"
+last_name = "Da Silva"
+age = 36
+profession = "Software Developer"
+years_experience = 5
+
+greeting = (f"Hello my name is {first_name} {last_name}")
+
+print(greeting) 
+# Desired output: Hello my name is Alana Da Silva
+
+print(f"I am {age} years old") 
+# Desired output: I am 36 years old
+
+print("I work as a {}.".format(profession))
+# Desired output: I work as a Software Developer.
+
+exp_string = ("I have worked in the field for {} years.".format(years_experience))
+print(exp_string)
+# Desired output: I have worked in the field for 5 years.
+
+print(f"I started in the field when I was {age - years_experience} years old.")
+# Desired output: I started in the field when I was 31 years old.
+
+my_string = "This is my string"
+upper_string = my_string.upper()
+print(upper_string)
